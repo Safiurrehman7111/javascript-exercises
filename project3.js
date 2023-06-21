d6af8db58e517b080a9dd6f1ad660bd5181b1cfd
Safiurrehman7111 <Safiurrehman7111>
2023-06-21 16:45:55 +0500

    Add existing project files to Git

---
+++ project3.js
@@ -0,0 +1,9 @@
+let str = "Ali";
+console.log( "The lower case is : " + str.toLowerCase());
+console.log( "The Upper case is : " + str.toUpperCase());
+let array = ["a","l","i"] ;//this array is because for title case 
+let a = array[0].toUpperCase();
+let b = array[1].toLowerCase();
+let c = array[2].toLowerCase();
+console.log ("The Title case is:  " + a + b + c);
+
