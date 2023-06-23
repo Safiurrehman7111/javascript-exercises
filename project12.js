9bf678cdef2badc046c631d44632697b32aabdac
Safiurrehman7111 <Safiurrehman7111>
2023-06-23 16:16:48 +0500

    project12.js

---
+++ project12.js
@@ -0,0 +1,13 @@
+//Project No 12
+/* Greetings: Start with the array you used in Exercise 11,
+ but instead of just printing each person’s name,
+ print a message to them. The text of each message should be the same,
+ but each message should be personalized with the person’s name. */
+ 
+let names = ["Ali", "Hamza", "Usman" ,"Ahmed", "Zain"] ;
+let message = "One of my best friend is  "
+console.log( message + names[0]);
+console.log(message + names[1]);
+console.log(message + names[2]);
+console.log(message + names[3]);
+console.log(message + names[4]);
