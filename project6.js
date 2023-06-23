02088bb0d7bf88f7064e15d9458a045dc49f7148
Safiurrehman7111 <Safiurrehman7111>
2023-06-23 10:52:31 +0500

    project6.js

---
+++ project6.js
@@ -0,0 +1,9 @@
+//Project No 6
+/*Stripping Names: Store a person’s name, and include some whitespace characters at the beginning
+ and end of the name. Make sure you use each character combination, "\t" and "\n", at least once.
+  Print the name once, so the whitespace around the name is displayed. 
+Then print the name after striping the white spaces.*/
+
+let a = "\t\n Ali \t\n"; //name with whitespace characters
+console.log(a);
+console.log(a.trim()); // removing white spaces
