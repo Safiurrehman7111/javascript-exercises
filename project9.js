6962224c2cd665d6f08af6a85eac029b1a235e38
Safiurrehman7111 <Safiurrehman7111>
2023-06-23 11:19:06 +0500

    project9.js

---
+++ project9.js
@@ -0,0 +1,7 @@
+//Project No 9
+/*Favorite Number: Store your favorite number in a variable.
+Then, using that variable, create a message that reveals your favorite number. Print that message.
+*/
+var fvrtNumber = 56;
+var message = "My Favorite number is " + fvrtNumber;
+console.log(message);
