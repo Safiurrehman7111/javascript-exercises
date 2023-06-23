743fd5e82dab016b80d06526cefc911d9c19d4f5
Safiurrehman7111 <Safiurrehman7111>
2023-06-23 11:26:51 +0500

    project10.js

---
+++ project10.js
@@ -0,0 +1,20 @@
+//Project No 10
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+ If you don’t have anything specific to write because your programs are too simple at this point, 
+just add your name and the current date at the top of each program file.
+ Then write one sentence describing what the program does.*/
+
+//Project no 8
+//Safi Ur Rehman 
+//23 June 2023
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+console.log(5+3);
+
+//Project no 9
+//Safi Ur Rehman 
+//23 June 2023
+var fvrtNumber = 56;
+var message = "My Favorite number is " + fvrtNumber;
+console.log(message);
