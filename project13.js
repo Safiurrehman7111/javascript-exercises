27da0dfd5844398ba7432ff7b7c4ea94184a1595
Safiurrehman7111 <Safiurrehman7111>
2023-06-23 16:34:19 +0500

    project13.js

---
+++ project13.js
@@ -0,0 +1,13 @@
+// Project No 13
+/*Your Own Array: Think of your favorite mode of transportation,
+such as a motorcycle or a car,
+and make a list that stores several examples.
+Use your list to print a series of statements about these items,
+such as “I would like to own a Honda motorcycle.”*/
+
+let transportationArray = ["Civic Car", "Elantra Car", "Sonata Car", "Honda Bike", "Road Prince Bike"];;
+console.log("I would like to go on " + transportationArray[3]);
+console.log("I dont like " + transportationArray[4]);
+console.log("My dream car is " + transportationArray[2]);
+console.log("Ali has bought " + transportationArray[0]);
+console.log("What is the price of " + transportationArray[1]);
