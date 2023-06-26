047bdd089ce806f307d24bfe7f6cf1d73da30ef8
Safiurrehman7111 <Safiurrehman7111>
2023-06-26 13:55:00 +0500

    project21.js

---
+++ project21.js
@@ -0,0 +1,10 @@
+//Project No 21
+/* They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items.*/
+ let obj = {
+    city : "Lahore",
+    mountain : "Mount Everest",
+    river : "Indus",
+    language : "Urdu"
+ };
+ console.log(obj);
