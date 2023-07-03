461d8cee0d0b10de4021164c4e8c8b0f8d91d36a
Safiurrehman7111 <Safiurrehman7111>
2023-07-03 11:19:30 +0500

    project23.js

---
+++ project23.js
@@ -0,0 +1,30 @@
+//Project No 23 
+/* Conditional Tests: Write a series of conditional tests.
+Print a statement describing each test and your prediction for the results of each test. 
+Your code should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+let car = 'subaru';
+console.log("If Car is subaru then print true: ",car == 'subaru');
+let marks = 75;
+console.log("If marks is not 75 then print false: ", marks == 100);
+let age = 20; 
+console.log("If age is 20 then print true: ",age== 20);
+let subject = "Math";
+console.log("If subject is not math then print false: ", subject == "Urdu");
+let level = 1;
+console.log( "If level is 1 then print true: ",level== 1);
+let bowler = "Naseem Shah";
+console.log("If bowler is not naseem then print false: ", bowler == "Amir");
+let fname = "Safi";
+console.log("If fname is Safi then print true: ",fname == "Safi");
+let game = "Pubg";
+console.log("If game is not Pubg then print False: ", game == "Angry Birds" );
+let lname = "ur Rehman";
+console.log("If lname is Ur rehman then print true : ", lname == "ur Rehman");
+let batsman = "Babar Azam";
+console.log("If batsman is not babar azam then print false: ", batsman == "Ali");
